4479f20717b93ab4ad689e9711d2eaa3ebef3f2b
Ifunanya <williamifunanya@gmail.com>
2023-09-15 09:43:22 +0100

    Adds CreateProjectCommandHandler

---
+++ TMS.Application/CommandHandlers/Project/CreateProjectCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace TMS.Application.CommandHandlers.Project
+{
+    using MediatR;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Commands.Project;
+    using Repository;
+
+    public class CreateProjectCommandHandler 
+                       : IRequestHandler<CreateProjectCommandParameter, bool>
+    {
+        private readonly IProjectRepository _projectRepository;
+        public CreateProjectCommandHandler(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<bool> Handle(
+                    CreateProjectCommandParameter request, 
+                    CancellationToken cancellationToken)
+        {
+            return await _projectRepository.CreateProjectAsyn(request.Name, request.Description);
+        }
+    }
+}
