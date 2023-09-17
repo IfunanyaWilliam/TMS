a2d8cfd9522adae57e42119bac172bed91522670
Ifunanya <williamifunanya@gmail.com>
2023-09-17 12:31:38 +0100

    Adds AppTaskController

---
+++ TMS.Api/Controllers/AppTaskController.cs
@@ -0,0 +1,21 @@
+﻿namespace TMS.Api.Controllers
+{
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppTaskController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public AppTaskController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+    }
+}
