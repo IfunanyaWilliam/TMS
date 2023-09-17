d009cf97eb1b7824698f7e41a86a12fad6579bd1
Ifunanya <williamifunanya@gmail.com>
2023-09-17 23:59:10 +0100

    Adds DeleteAppTaskCommandHandler

---
+++ TMS.Application/CommandHandlers/AppTask/DeleteAppTaskCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace TMS.Application.CommandHandlers.AppTask
+{
+    using MediatR;
+    using Commands.AppTask;
+    using Repository;
+    using System.Threading.Tasks;
+    using System.Threading;
+
+    public class DeleteAppTaskCommandHandler
+                : IRequestHandler<DeleteAppTaskCommandParameter, bool>, IMediatRHandler
+    {
+        private readonly IAppTaskRepository _appTaskRepository;
+
+        public DeleteAppTaskCommandHandler(IAppTaskRepository appTaskRepository)
+        {
+            _appTaskRepository = appTaskRepository;
+        }
+
+        public async Task<bool> Handle(
+                        DeleteAppTaskCommandParameter request, 
+                        CancellationToken cancellationToken)
+        {
+            return await _appTaskRepository.DeleteTaskAsync(id: request.Id);
+        }
+    }
+}
---
+++ TMS.Application/Commands/AppTask/DeleteAppTaskCommandParameter.cs
@@ -0,0 +1,14 @@
+﻿namespace TMS.Application.Commands.AppTask
+{
+    using MediatR;
+
+    public class DeleteAppTaskCommandParameter : IRequest<bool>
+    {
+        public DeleteAppTaskCommandParameter(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+}
