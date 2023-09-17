f215f74b4fe262a2dc52773bb7ad0be78b4f0248
Ifunanya <williamifunanya@gmail.com>
2023-09-17 16:07:13 +0100

    Adds RemoveAppTaskFromProjectCommandHandler

---
+++ TMS.Application/CommandHandlers/Project/RemoveAppTaskFromProjectCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace TMS.Application.CommandHandlers.Project
+{
+    using MediatR;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Commands.Project;
+    using Repository;
+    internal class RemoveAppTaskFromProjectCommandHandler
+                : IRequestHandler<RemoveAppTaskFromProjectCommandParameter, bool>, IMediatRHandler
+    {
+        private readonly IProjectRepository _projectRepository;
+
+        public RemoveAppTaskFromProjectCommandHandler(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<bool> Handle(
+                    RemoveAppTaskFromProjectCommandParameter request, 
+                    CancellationToken cancellationToken)
+        {
+            return await _projectRepository.RemoveAppTaskFromProject(
+                projectId: request.ProjectId,
+                appTaskId: request.AppTaskId);
+        }
+    }
+}
