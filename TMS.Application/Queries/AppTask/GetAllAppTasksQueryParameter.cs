5bf2ab1c0c1e270e642cefee7a0c71916fd86e7f
Ifunanya <williamifunanya@gmail.com>
2023-09-17 14:47:00 +0100

    Adds GetAllAppTasksQueryParameter

---
+++ TMS.Application/Queries/AppTask/GetAllAppTasksQueryParameter.cs
@@ -0,0 +1,25 @@
+﻿namespace TMS.Application.Queries.AppTask
+{
+    using MediatR;
+    using Domain.AppTask;
+
+    public class GetAllAppTasksQueryParameter : IRequest<IEnumerable<AppTask>>
+    {
+        public GetAllAppTasksQueryParameter(
+            int pageNumber,
+            int pageSize,
+            string searchParam)
+        {
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            SearchParam = searchParam;
+        }
+
+
+        public int PageNumber { get; }
+
+        public int PageSize { get; }
+
+        public string SearchParam { get; }
+    }
+}
