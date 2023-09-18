9188186fae9eedf47c9c9dfd7a6ab11361e12d6b
Ifunanya <williamifunanya@gmail.com>
2023-09-18 22:00:41 +0100

    Updates UserTaskRepository

--- TMS.Application/CommandResults/UserTask/CreateUserTaskCommandResult.cs
+++ TMS.Application/CommandResults/UserTask/CreateUserTaskCommandResult.cs
@@ -1,16 +1,27 @@
-﻿namespace TMS.Application.CommandResults.UserTask
+﻿using TMS.Domain.AppTask;
+
+namespace TMS.Application.CommandResults.UserTask
 {
     public class CreateUserTaskCommandResult
     {
         public CreateUserTaskCommandResult(
             Guid userTaskId,
-            Guid userId) 
+            Guid userId,
+            string fullName,
+            IEnumerable<AppTask> tasks) 
         {
             UserTaskId = userTaskId;
             UserId = userId;
+            FullName = fullName;
+            Tasks = tasks;
         }
 
         public Guid UserTaskId { get; }
+
         public Guid UserId { get; }
+
+        public string FullName { get; }
+
+        public IEnumerable<AppTask> Tasks { get; }
     }
 }
