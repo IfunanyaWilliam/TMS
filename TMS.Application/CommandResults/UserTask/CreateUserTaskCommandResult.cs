ff1276e233f2dcb9da5828adf77b0e38dbb23ac1
Ifunanya <williamifunanya@gmail.com>
2023-09-18 18:00:57 +0100

    Adds UserTaskRepository
    Adds CreateUserTaskCommandResult

---
+++ TMS.Application/CommandResults/UserTask/CreateUserTaskCommandResult.cs
@@ -0,0 +1,16 @@
+﻿namespace TMS.Application.CommandResults.UserTask
+{
+    public class CreateUserTaskCommandResult
+    {
+        public CreateUserTaskCommandResult(
+            Guid userTaskId,
+            Guid userId) 
+        {
+            UserTaskId = userTaskId;
+            UserId = userId;
+        }
+
+        public Guid UserTaskId { get; }
+        public Guid UserId { get; }
+    }
+}
---
+++ TMS.Infrastructure/Repository/UserTaskRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace TMS.Infrastructure.Repository
+{
+    using Application.CommandResults.UserTask;
+    using Infrastructure.DbContext;
+    using Microsoft.EntityFrameworkCore;
+
+    public class UserTaskRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserTaskRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<CreateUserTaskCommandResult> CreateUserTaskAsync(
+                Guid userId)
+        {
+            var existingUserTask = await _context.UsersTasks.FirstOrDefaultAsync(x => x.UserId == userId);
+
+            if (existingUserTask != null)
+                return new CreateUserTaskCommandResult(
+                    userTaskId: existingUserTask.Id, userId: existingUserTask.UserId);
+
+            var userTask = new Entities.UserTask
+            {
+                UserId = userId
+            };
+
+            await _context.UsersTasks.AddAsync(userTask);
+            var result = await _context.SaveChangesAsync();
+
+            if (result > 0)
+            {
+                return new CreateUserTaskCommandResult(
+                    userTaskId: userTask.Id, userId: userTask.UserId);
+            }
+
+            return null;
+        }
+    }
+}
